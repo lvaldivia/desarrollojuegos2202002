329606fa78be770cd704c0e0d5fc9188b3970c7e
luis <valdivialuis1989@gmail.com>
2020-10-03 14:36:30 -0500

    move and turret

---
+++ Tanks/Source/ToonTanks/Pawns/Turret.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+
+#include "Turret.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATurret : public ABasePawn
+{
+	GENERATED_BODY()
+	private:
+		UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Combat",meta=(AllowPrivateAccess="true"))
+		float FireRate = 2.0f;
+		UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Combat",meta=(AllowPrivateAccess="true"))
+		float FireRange = 500.0f;
+		void CheckFireCondition();
+		FTimerHandle FireRateTimeHandle;
+		ATank* Player;
+		float ReturnDistanceToPlayer();
+	protected:
+		virtual void BeginPlay()override;
+	public:
+		virtual void Tick(float DeltaTime) override;
+};
