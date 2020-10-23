330aae88063541e2fe8c987112497859eaf4ad51
luis <valdivialuis1989@gmail.com>
2020-10-23 16:49:20 -0500

    tank fix

---
+++ Tank/Source/ToonTanks/PawnTurret.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PawnTurret.h"
+#include "Kismet/GameplayStatics.h"
+#include "PawnTank.h"
+
+
+
+void APawnTurret::BeginPlay()
+{
+    Super::BeginPlay();
+
+    GetWorld()->GetTimerManager().SetTimer(FireRateTimerHandle, this, &APawnTurret::CheckFireCondition, FireRate, true);
+
+    PlayerPawn = Cast<APawnTank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
+
+// Called every frame
+void APawnTurret::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void APawnTurret::CheckFireCondition() 
+{
+    if(!PlayerPawn)
+    {
+        return;
+    }
+    if(ReturnDistanceToPlayer() <= FireRange)
+    {
+        // Fire
+        UE_LOG(LogTemp, Error, TEXT("Fire Condition Checked"));
+    }
+   
+}
+
+float APawnTurret::ReturnDistanceToPlayer() 
+{
+    if (!PlayerPawn)
+    {
+        0.0f;
+    }
+
+    return FVector::Dist(PlayerPawn->GetActorLocation(), GetActorLocation());
+}
--- Tanks/Intermediate/Build/Win64/ToonTanksEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Luis/Downloads/ToonTanksProjectSetup_4.25/ToonTanksProjectSetup_4.25/ToonTanks/Intermediate/Build/Win64/ToonTanksEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
